f928d2c917b2c972e985b6b7b50ae29c29c099ef
Tomáš Reinert <reinerttomas@gmail.com>
2023-09-22 16:20:42 +0200

    send email via messenger

--- src/MessageHandler/SendWelcomeEmailHandler.php
+++ src/MessageHandler/SendWelcomeEmailHandler.php
@@ -2,14 +2,27 @@
 
 namespace App\MessageHandler;
 
+use App\Action\SendWelcomeEmailAction;
 use App\Message\SendWelcomeEmail;
+use Symfony\Component\Mailer\Exception\TransportExceptionInterface;
 use Symfony\Component\Messenger\Attribute\AsMessageHandler;
 
 #[AsMessageHandler]
 final class SendWelcomeEmailHandler
 {
+    public function __construct(
+        private readonly SendWelcomeEmailAction $sendWelcomeEmailAction,
+    ) {
+    }
+
+    /**
+     * @throws TransportExceptionInterface
+     */
     public function __invoke(SendWelcomeEmail $message): void
     {
+        // add some delay
         sleep(3);
+
+        $this->sendWelcomeEmailAction->execute($message->user);
     }
 }
